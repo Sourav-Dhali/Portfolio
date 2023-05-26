32f81732e43438dd29f8a4cb58ff9f44ff5a661f
sourav <souravd2397@gmail.com>
2023-05-26 21:27:56 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectComponent } from './project/project.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component: AboutComponent},
+  {path:'skills',component: SkillsComponent},
+  {path:'project',component: ProjectComponent},
+  {path:'resume',component: ResumeComponent},
+  {path:'contact',component: ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
